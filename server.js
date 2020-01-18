ca4037a22536d53fb4de087595b858e07a4c6446
davidgonzalezfx <dvdramos16@gmail.com>
2020-01-18 17:52:34 -0500

    Deploy to heroku

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Working");
 })
 
